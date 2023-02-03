612e427d03efe2c76f417781bbcdd02f3c11ba87
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-02-03 16:12:27 +0100

    fix: auth middleware variable fix

--- controllers/auth_middleware.js
+++ controllers/auth_middleware.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
     try {
-        const userId = await request.headers.authorization.split(" ")[0];
-        const token = await request.headers.authorization.split(" ")[1];
+        const userId = await req.headers.authorization.split(' ')[0];
+        const token = await req.headers.authorization.split(' ')[1];
         const user = jwt.verify(token, process.env.JWT_SECRET);
-        user.user_id = parseInt(user.user_id);
+        user.id = parseInt(user.id);
 
-        if (parseInt(userId) !== user.user_id) {
-            throw "err";
+        if (parseInt(userId) !== user.id) {
+            throw 'err';
         }
 
         req.user = user;
         next();
     } catch (err) {
         res.status(401).json({
-            error: new Error("Unauthorized request!")
+            error: new Error('Unauthorized request!')
         });
     }
 }
