c8312e69bfc87707d36ff3e92b7984e2a27b294d
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-02-03 16:11:54 +0100

    chore: clarify and modify mongoose schema

--- models/pic.js
+++ models/pic.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const PicSchema = new mongoose.Schema({
     user_id: { type: Number, required: true },
     user_avatar_url: { type: String, required: true },
+    user_login: { type: String, required: true }, // a user's login is their username/handle
     pic_url: { type: String, required: true },
-    pic_description: { type: String, default: '*no description*' },
+    pic_description: { type: String, required: true },
     pic_likes: { type: [Number], default: [] } // array of user_ids
 });
 
